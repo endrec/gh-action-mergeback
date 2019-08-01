b6d4e5efec3c2656d2510366f679e5e745650064
Endre Czirbesz <endre.czirbesz@rungway.com>
2019-08-01 17:22:43 +0100

    What about a PR?

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,29 @@ case $GITHUB_REF in
          *) echo "Not on master, do nothing."; exit 0;;
 esac
 
+
+# Let's create a PR
+payload=$(cat <<EOF 
+{
+  "base": "${BASE_BRANCH:=develop}",
+  "head": "master",
+  "title": "Merging back master"
+}
+EOF
+)
+output=$( { 
+status_code=$(curl --silent -i --output /dev/stderr \
+	--write-out "%{http_code}" \
+	-H "Authorization: token ${GITHUB_TOKEN}" \
+	-X POST https://api.github.com/repos/${GITHUB_REPOSITORY}/merges \
+	-d "${payload}")
+} 2>&1 )
+
+echo $output
+
+# Stop here for now
+exit 2
+
 payload=$(cat <<EOF 
 {
   "base": "${BASE_BRANCH:=develop}",
