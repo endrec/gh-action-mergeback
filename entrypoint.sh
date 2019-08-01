9452bd51fe2185577aae5d7c1e7ed806cc7a035e
Endre Czirbesz <endre.czirbesz@rungway.com>
2019-08-01 17:00:45 +0100

    aading some ddebug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+env | grep GITHUB
+
 # ASH is magic...
 case $GITHUB_REF in
   */master) echo "We are on master, let's merge.";;
@@ -16,13 +18,15 @@ payload=$(cat <<EOF
 EOF
 )
 
-{ 
+output=$( { 
 status_code=$(curl --silent -i --output /dev/stderr \
 	--write-out "%{http_code}" \
 	-H "Authorization: token ${GITHUB_TOKEN}" \
 	-X POST https://api.github.com/repos/${GITHUB_REPOSITORY}/merges \
 	-d "${payload}")
-} 2>&1
+} 2>&1 )
+
+echo $output
 
 if test ${status_code} -ne 200; then
     echo "The merge has failed with status code ${status_code}"
